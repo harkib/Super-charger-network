6ddf31c17377e311afc1f32030574fb3b0ad5b06
ljnRudy <ljnrudy@gmail.com>
2020-03-18 17:39:42 -0700

    initialize some variables  --rudy

--- main.cpp
+++ main.cpp
@@ -4,6 +4,16 @@ using namespace std;
 
 int main(int argc, char** argv)
 {
-    
+    std::string start_charger_name;
+    std::string end_charger_name;
+    double full_charge = 320.00;
+    double speed = 105.00;
+    double Earth_radius = 6356.752;
+    double distance = 0;/*distance  between two chargers*/
+    /*look into network array,
+    find lat and lon of two chargers,
+    calculate the distance
+    */
+
     return 0;
 }
