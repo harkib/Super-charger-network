c762d443940b5169cdbfb99b404a7f95ee5bd851
harkib <harkib@sfu.ca>
2020-04-20 16:19:57 -0700

    bug fix and using max_rate for charge h

--- main.cpp
+++ main.cpp
@@ -59,7 +59,7 @@ map< int, map< int, double>> gen_distance_map(int n){
     //map< network_index1, map< network_index2, double> > distance;
     map< int, map< int, double>> distance_map;
     
-    double arc_distance;
+    double arc_distance;  
     for (int i = 0; i < n; i++){
         for (int j = 0; j < n; j++){
             if (i!=j){
@@ -72,32 +72,39 @@ map< int, map< int, double>> gen_distance_map(int n){
     return distance_map;
 }
 
+ 
 
+double h(int i, int j, double rate_max, double curr_charge){
+ 
+    //minimum drive time
+    double h_drive = get_arc_distance(i,j)/speed; 
 
-double h(int i, int j, double rate_min){
-
-
-  //return get_arc_distance(i,j)/speed;
-
-    double h_drive = get_arc_distance(i,j)/speed;
-    double h_charge = (get_arc_distance(i,j) - full_charge)/rate_min;
+    //minimum charge time 
+    //double possible_charging_at_curr = full_charge - curr_charge;
+    //double h_charge = (get_arc_distance(i,j) - full_charge)/rate_min + possible_charging_at_curr/network[i].rate;
+    double h_charge = (get_arc_distance(i,j) - curr_charge)/rate_max;
+    if (h_charge < 0){
+        h_charge = 0;
+    }
     return h_drive + h_charge;
-    
+     
 } 
 
-double get_rate_min(int n){
-    double rate_min = 999;
+double get_rate_max(int n){
+    double rate_max = 0;
     for (int i = 0; i < n; i++){
-        if(rate_min > network[i].rate){
-            rate_min = network[i].rate;
+        if(rate_max < network[i].rate){
+            rate_max = network[i].rate;
         }
     }
-    return rate_min;
+    return rate_max;
 }
 
 vector<pair<string,double>> A_star(map< int, map< int, double>> distance_map,int n, std::string start,std::string goal){
-    double time_step = .01; //hours // for charging 
-    double rate_min = get_rate_min(n);
+    double time_step = .1; //hours // for charging 
+    double rate_max = get_rate_max(n);
+    cout << "Running A-star... with time_step = "<< time_step <<", rate_max = "<<rate_max << endl;
+    
     priority_queue<node, vector<node>, compare_node_f> pQueue;
 
     // look up index of start node name
@@ -112,6 +119,7 @@ vector<pair<string,double>> A_star(map< int, map< int, double>> distance_map,int
         }
     }
 
+    //create root node
     node root;
     root.name = network[start_i].name;
     root.network_index = start_i;
@@ -121,7 +129,7 @@ vector<pair<string,double>> A_star(map< int, map< int, double>> distance_map,int
     root.car_charge = full_charge;
     root.time = 0;
     root.path.push_back(std::make_pair(root.name,0));
-    root.f = h(start_i,goal_i,rate_min);
+    root.f = h(start_i,goal_i,rate_max, root.car_charge);
     pQueue.push(root);
 
     while (!pQueue.empty()){
@@ -148,6 +156,7 @@ vector<pair<string,double>> A_star(map< int, map< int, double>> distance_map,int
 
             //reachable nodes 
             if (distance_map[curr.network_index][i] <= curr.car_charge && curr.network_index != i){
+
                 //ckeck for looping 
                 bool visted = false; 
                 int path_len = curr.path.size();
@@ -157,6 +166,7 @@ vector<pair<string,double>> A_star(map< int, map< int, double>> distance_map,int
                     }
                 }
 
+                //if not visited
                 if(!visted){
                     node x;
                     x.name = network[i].name;
@@ -168,31 +178,61 @@ vector<pair<string,double>> A_star(map< int, map< int, double>> distance_map,int
                     x.time = curr.time + distance_map[curr.network_index][i]/speed;
                     x.path = curr.path;
                     x.path.push_back(std::make_pair(x.name,0));
-                    x.f = x.time +h(i,goal_i,rate_min);
-                    
+                    x.f = x.time +h(i,goal_i,rate_max,x.car_charge);
+                    //x.f = x.time +h(i,goal_i,rate_min,full_charge);
                     pQueue.push(x);
 
                     //cout << "added reachable node" << endl;
-                }
-            } else {
+                }          
+            } 
+            //rechable with charging  
+            // else if(distance_map[curr.network_index][i] <= full_charge && curr.network_index != i){
+
+            //     //ckeck for looping 
+            //     bool visted = false; 
+            //     int path_len = curr.path.size();
+            //     for (int k = 0; k < path_len; k++){
+            //         if(curr.path[k].first == network[i].name){
+            //             visted = true;
+            //         }
+            //     }
+
+            //     //if not visited
+            //     //stays at same charger but charges minimum to get to charger i 
+            //     if(!visted){
+            //         double charge_needed = distance_map[curr.network_index][i] - curr.car_charge;
+            //         double time_needed = charge_needed/curr.rate;
+
+            //         node x = curr;
+            //         x.car_charge += charge_needed;
+            //         x.time +=  time_needed;
+            //         x.path[x.path.size()-1].second += time_needed;
+            //         x.f = x.time +h(x.network_index,goal_i,rate_min,x.car_charge);
+                    
+            //         pQueue.push(x);
+            //     }
+
+            // }
+            else {
                 //cout << "not reachble" << endl;
             }
         }
 
         //add charging nodes
-        if (curr.car_charge < full_charge) {
+        if (curr.car_charge < full_charge) { 
             node x = curr;
             x.time += time_step;
-            x.car_charge += time_step*x.rate;
+            x.car_charge += time_step*x.rate;  
             x.path[x.path.size()-1].second += time_step;
             if (x.car_charge > full_charge){
-                int over_time = (x.car_charge - full_charge)/x.rate;
+                float over_time = (x.car_charge - full_charge)/x.rate;
                 x.car_charge = full_charge;
                 x.time -= over_time;
                 x.path[x.path.size()-1].second -= over_time;
 
             }
-            x.f = x.time + h(x.network_index, goal_i,rate_min);
+            x.f = x.time + h(x.network_index, goal_i,rate_max,x.car_charge);
+            //x.f = x.time + h(x.network_index, goal_i,rate_min,full_charge);
             pQueue.push(x);
 
         }
@@ -214,6 +254,46 @@ bool valid_user_input(std::string location, int n){
     return found;
 }
 
+void check_path(vector<pair<string,double>> path, map< int, map< int, double>> distance_map, int n){
+    vector<pair<int,double>> path_indexed;
+    double remaining_charge = full_charge;
+    double drive_dis = 0;
+
+    //convert names to netwrok indexs
+    for (int k = 0; k < path.size(); k++){
+        for (int j = 0; j < n; j++){
+            if (network[j].name == path[k].first){
+                path_indexed.push_back(make_pair(j,path[k].second));
+            }
+        }
+    }
+
+    //compute
+    int curr, next;
+    for (int k = 0; k < path_indexed.size() -1; k++){
+
+        //compute drive distance
+        curr = path_indexed[k].first;
+        next = path_indexed[k+1].first;
+        drive_dis += distance_map[curr][next];
+
+        //compute remaining charge
+        remaining_charge -= distance_map[curr][next];
+        cout <<"Drove "<<distance_map[curr][next] << " km to "<< path[k+1].first <<", charge (km) reamining = "<<remaining_charge << endl;
+        
+
+        if(remaining_charge < 0){
+            cout <<"Charge below Zero!" << endl;
+        }
+        remaining_charge += network[next].rate*path_indexed[k+1].second;
+        cout <<network[next].name <<" Charger speed: "<< network[next].rate << ",Charged to (km):" << remaining_charge <<endl;
+    }
+
+
+    cout << "Remaining charge (km) (should be close to 0): " << remaining_charge << endl; 
+    cout << "Drive distance (compare with google): " << drive_dis << endl;
+
+}
 
 
 int main(int argc, char** argv)
@@ -246,11 +326,11 @@ int main(int argc, char** argv)
     map< int, map< int, double>> distance_map = gen_distance_map(n);
 
     //cout << network[0].name << ", " << network[1].name << ": " << distance_map[0][1] << endl;
-    cout << "Running A-star..." << endl;
     vector<pair<string,double>> path = A_star(distance_map,n, start_charger_name, end_charger_name);
 
     //print path
     print_path(path);
+    check_path(path, distance_map,n);
 
     //debug
     // look up index of start node name
